e1188be67bd44153d2ef20d615798ff72e1ea23a
Oleg Sushkov <sushkov@gmail.com>
2016-08-03 18:03:43 +1000

    more work

--- src/ParallelMatrixMultiply.cpp
+++ src/ParallelMatrixMultiply.cpp
@@ -1,5 +1,6 @@
 
 #include "ParallelMatrixMultiply.hpp"
+#include "cuda/TaskExecutor.hpp"
 #include <cassert>
 #include <condition_variable>
 #include <mutex>
@@ -12,6 +13,8 @@ struct ParallelMatrixMultiply::ParallelMatrixMultiplyImpl {
 
   vector<AsyncTask> tasks;
   vector<thread> workers;
+
+  cuda::TaskExecutor taskExecutor;
 
   ParallelMatrixMultiplyImpl(unsigned numStreams) : shouldStop(false) {
     assert(numStreams > 0);
@@ -30,6 +33,8 @@ struct ParallelMatrixMultiply::ParallelMatrixMultiplyImpl {
   void createWorkers(unsigned numWorkers) {
     for (unsigned i = 0; i < numWorkers; i++) {
       workers.emplace_back([this, i] {
+        cuda::StreamId streamId = taskExecutor.CreateStream();
+
         while (true) {
           std::unique_lock<std::mutex> lk(m);
 
@@ -47,15 +52,18 @@ struct ParallelMatrixMultiply::ParallelMatrixMultiplyImpl {
 
             lk.unlock();
 
-            doTask(task);
+            doTask(task, streamId);
           }
         }
       });
     }
   }
 
-  void doTask(const AsyncTask &task) {
-    auto result = task.lhs * task.rhs;
+  void doTask(AsyncTask &task, cuda::StreamId streamId) {
+    EMatrix result(task.lhs.rows(), task.rhs.cols());
+    cuda::Task ctask(math::GetMatrixView(task.lhs), math::GetMatrixView(task.rhs), math::GetMatrixView(result));
+    taskExecutor.ExecuteTask(ctask, streamId);
+
     task.resultCallback(result);
   }
 
